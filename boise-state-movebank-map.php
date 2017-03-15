0e08bb60229732153c541e843267fdaf032dd8de
davidmlentz <davidmlentz@gmail.com>
2017-03-15 14:26:41 -0600

    Fixing an a11y error and cleaning up some code

--- boise-state-movebank-map.php
+++ boise-state-movebank-map.php
@@ -2,20 +2,13 @@
 /*
 Plugin Name: Boise State Movebank Map
 Description: A custom plugin by Boise State for displaying Movebank animal tracking data
-Version: 0.0.3
+Version: 0.0.4
 */
 
 function bsu_movebank_shortcode() {
 
 	wp_register_script( 'params', plugins_url( '/params.js', __FILE__ ) );
 	wp_enqueue_script('params');
-
-	// Unclear if this is necessary. Doesn't appear to change anything if we comment it out.
-// 	wp_register_style( 'smoothness', 'https://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css' );
-//	wp_enqueue_style( 'smoothness' );
-
-// 	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' );
-// 	wp_enqueue_script( 'jquery' );
 
 	wp_register_script( 'jqueryui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js' );
 	wp_enqueue_script( 'jqueryui' );
@@ -25,8 +18,6 @@ function bsu_movebank_shortcode() {
 
 	wp_register_script( 'movebank',  plugins_url( '/movebank.js', __FILE__ ) );
 	wp_enqueue_script( 'movebank' );
-
-//	$str = '<div id="map-canvas" style="width: 600px; height: 700px"></div><div id="time-display-div"><input type="text" id="time-display" style="border: none; font-weight: bold; color: #FFFFFF; background: transparent; margin-top: 7px; width: 0px"></input></div></div>';
 
 	$str = '<div id="map-canvas" style="width: 600px; height: 700px"></div></div>';
 
